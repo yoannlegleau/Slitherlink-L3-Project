df1ae1a25cf41fe6a17b494030b7a0989d9b2414
varix33 <antoine.bruneau@outlook.fr>
2023-03-01 12:01:03 +0100

    début création techniques

---
+++ src/main/java/fr/slitherlink/aide/Aide.java
@@ -0,0 +1,13 @@
+/* MEMO
+id_aide
+ajout dans un tableau des id
+techniques à identifier pour la surbrillance (26 et 27)
+surbrillance de la zone ou des chiffres en fonction de la technique
+affichage de la surbrillance direct après l'affichage de la technique (lors d'un nouvel appui)
+score à voir
+refaire les images
+lier le xml (ressources/fr/slitherlink/techniques)
+mise en place de l'anglais
+générer une aide lors de l'appui et stocker son instance
+choix de la technique avec l'id le plus petit possible
+*/
