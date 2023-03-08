5876d7e8cb3221131b3faa240601bcba964b8893
yoann LE GLEAU <81438255+yoannlegleau@users.noreply.github.com>
2023-03-08 23:04:20 +0100

    navigation menu->jeu implementation(vide) de l'interface de jeux

---
+++ src/main/java/fr/slitherlink/app/LevelPlaySceen.java
@@ -0,0 +1,14 @@
+package fr.slitherlink.app;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+/**
+ * @author LE GLEAU Yoann
+ * @version 1, 08/03/2023
+ */
+public class LevelPlaySceen {
+
+    private static final int levelid = 1;
+
+}
---
+++ src/main/java/fr/slitherlink/app/Slitherlink.java
@@ -0,0 +1,31 @@
+package fr.slitherlink.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Slitherlink extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Slitherlink.class.getResource("gui/mainMenu/menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setWidth(Screen.getPrimary().getBounds().getWidth()/1.5);
+        stage.setHeight(Screen.getPrimary().getBounds().getHeight()/1.5);
+        stage.setTitle("SlitherLink!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void loadFxml(String path){
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
