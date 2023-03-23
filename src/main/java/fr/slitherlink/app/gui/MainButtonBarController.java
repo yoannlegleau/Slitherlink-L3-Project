840bdeec17b282a29fd84a07ae3abab84b6602d2
TheMisterPenguin <ange.despert@gmail.com>
2023-03-23 11:23:50 +0100

    Refactoring

---
+++ src/main/java/fr/slitherlink/app/gui/MainButtonBarController.java
@@ -0,0 +1,15 @@
+package fr.slitherlink.app.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ButtonBar;
+
+public class MainButtonBarController {
+
+    @FXML 
+    private ButtonBar bar;
+
+    public ButtonBar getBar() {
+        return bar;
+    }
+    
+}
