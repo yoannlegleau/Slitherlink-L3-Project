961948b8c53dae4c60e162aa0d7e8ad23b2da8aa
varix33 <antoine.bruneau@outlook.fr>
2023-04-06 10:41:00 +0200

    modif listTechnic

--- src/test/java/fr/slitherlink/game/help/ListTechnicTest.java
+++ src/test/java/fr/slitherlink/game/help/ListTechnicTest.java
@@ -8,6 +8,7 @@ public class ListTechnicTest {
 
     @Test
     public static void main(String[] args) {
-        assertTrue(ListTechnic.generate().listTechnic.size() > 0);
+        assertTrue(ListTechnic.generate().getTechnic(0).getTitle().equals("adjacent 0 et 3"));
+        assertTrue(ListTechnic.generate().getTechnic(24).getTitle().equals("3 adjacent à deux 1 en diagonale"));
     }
 }
