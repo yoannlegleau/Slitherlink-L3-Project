18b85c4cc33894ca264611411ae40a874d3ac64d
Antoine Lancelot <alancelot27@gmail.com>
2023-04-10 17:21:33 +0200

    💄 fix UI bugs + add new icon

--- src/main/java/fr/slitherlink/app/gui/NavBarController.java
+++ src/main/java/fr/slitherlink/app/gui/NavBarController.java
@@ -1,7 +1,9 @@
 package fr.slitherlink.app.gui;
 
 import fr.slitherlink.app.Slitherlink;
+import fr.slitherlink.app.gui.popup.ProfilPopup;
 import fr.slitherlink.app.gui.popup.SettingsPopup;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
@@ -9,8 +11,6 @@ import javafx.scene.layout.Pane;
 import java.io.IOException;
 
 public class NavBarController {
-
-
 
     @FXML
     private void onSettingsButtonClick() {
@@ -25,5 +25,16 @@ public class NavBarController {
         }
     }
 
-
+    @FXML
+    public void onAccountButtonClick() {
+        try {
+            FXMLLoader loader = new FXMLLoader(Slitherlink.class.getResource("gui/popup/profil-popup.fxml"));
+            Pane root = loader.load();
+            ProfilPopup profilPopup = new ProfilPopup();
+            profilPopup.addContent(root);
+            profilPopup.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
